e81afa16cbd71297d6e12b0a72c6ed84dbf50fe5
Steven Ly <1stevenly92@gmail.com>
2020-05-22 13:44:36 -0700

    added readme

--- Bakery/Controllers/VendorController.cs
+++ Bakery/Controllers/VendorController.cs
@@ -9,24 +9,42 @@ namespace Bakery.Controllers
     public class VenderController : Controller
     {
 
-       [HttpGet("/vendor")]
+        [HttpGet("/vendor")]
         public ActionResult Index()
         {
             List<Vendor> allVendor = Vendor.GetAll();
             return View(allVendor);
         }
 
-      [HttpGet("/vendor/new")]
-      public ActionResult New()
-      {
-        return View();
-      }
+        [HttpGet("/vendor/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
 
-       [HttpPost("/vendor")]
-        public ActionResult Create(string name,string description)
+        [HttpPost("/vendor")]
+        public ActionResult Create(string name, string description)
         {
             Vendor myItem = new Vendor(name, description);
             return RedirectToAction("Index");
         }
+
+        // [HttpPost("/vendor/{Id}/order/new")]
+        // public ActionResult Create(string description, string date, int price, string title)
+        // {
+        //   Order myOrder = new Order (description, date, price, title);
+        //   return View(myOrder);
+        // }
+
+        // [HttpGet("/vendor/{orderId}/order/{orderId}")]
+        // public ActionResult Create(string order)
+        // {
+            
+        //     List<Order> addOrder= Vendor.AddOrder(order);
+            
+           
+           
+        //     return View(addOrder);
+        // }
     }
 }
---
+++ README.md
@@ -0,0 +1,49 @@
+ # __Pierrer's Vendor
+
+#### _Friday Practice _
+
+#### By _StevenL May 22, 2020_
+
+## Description
+
+### _ A program that stores user inputs of vendor names,descriptions and orders _
+
+## Setup/Installation Requirements
+
+* Clone this repository.
+* Open in the text editor of your choice
+* Open terminal and run dotnet build and dotnet watch run
+
+
+
+## Specs
+| Spec | Input | OutPut |
+|:----------- | :-----------------| :---------------|
+|  | Input Vendor name: "Bob"  and Description: "cat" | Output: Bob |
+| Lower/Uppcase doesnt matter | Snput: "dog" Sentence: "DoG" | Output 1 
+| Count is 0 | Input : "A" and Sentence: "P" | Output 0
+| Ignores punctuation | Input: cat and Sentence: OMG LOOK ITS A CAT! | Output 1
+
+
+## Known Bugs
+
+No
+
+## Support and contact details
+
+If you have any suggestions or questions please message me on github
+
+## Technologies Used
+
+* C#
+ * .Net
+* MVC
+* RESTful Design
+* Git
+
+
+### License
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2020 **_StevenLy_**
